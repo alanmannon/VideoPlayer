7ea2944941b918172dea465524e35fee2f02d654
hubert <huby40@gmail.com>
2020-10-26 22:05:06 -0400

    adds playlist selector/adder

--- scripts-cookie.js
+++ scripts-cookie.js
@@ -1,6 +1,8 @@
 // COOKIES LIBRARY
 document.cookie = "path=/";
 
+var selectPlaylist = "";
+
 function parseArray(array) {
   var finalarray = [];
   if (urlNumber === 1) {
@@ -53,7 +55,7 @@ var myWidget = cloudinary.createUploadWidget({
       //First Upload
       document.cookie = "url=" + JSON.stringify(result.info);
     } else if (previousCookie != undefined) {
-      //Subsequent uploads
+      //Subsequent uploads - WILL BREAK IF THERE ARE PLAYLISTS
       var newCookie = previousCookie + ", " + JSON.stringify(result.info);
       document.cookie = newCookie;
 
@@ -63,7 +65,7 @@ var myWidget = cloudinary.createUploadWidget({
     console.log('Done! Here is the image info: ', result.info);
     console.log('Cookie is:', document.cookie);
   }
-})
+});
 
 document.getElementById("upload_widget").addEventListener("click", function () {
   myWidget.open();
@@ -73,30 +75,31 @@ document.getElementById("upload_widget").addEventListener("click", function () {
 //Sets some pieces of DATA into a called cookieKeys
 // if (data === true) {
 var thumbnailString = "";
-data.forEach(function(index) {
-  thumbnailString += `<img src=${index.thumbnail_url} href=${index.url} style="width: 200px; height: 150px; margin: 30px; border: 2px solid rgb(173, 253, 47); border-radius: 7px;"></img>` ; 
+data.forEach(function (index) {
+  thumbnailString += `<img src=${index.thumbnail_url} href=${index.url} style="width: 200px; height: 150px; margin: 30px; border: 2px solid rgb(173, 253, 47); border-radius: 7px;"></img><button style="height: 30px; width: 30px;" onclick="addToPlaylist('${index.thumbnail_url}')">+</button>`;
 });
 
 document.getElementById("thumbnail-container").innerHTML = thumbnailString;
 
 
 
-var playlistArray = [];
 
 
 //PLAYLIST CODE
 function addToPlaylist(thumbnailUrl) {
   var entry = data.find(video => video["thumbnail_url"] === thumbnailUrl);
-  // entry["playlistName"] = playlistName;
-
+  entry["playlistName"] = selectPlaylist;
+  var playlistArray = [];
   playlistArray.push(JSON.stringify(entry));
   console.log(playlistArray);
-  //if there are no playlists before... 
-  // if (previousPlaylist() === undefined) {
-  document.cookie = "playlist=" + playlistArray;
-  // } else {
-  //   document.cookie = document.cookie + playlistArray;
-  // }
+  // //if there are no playlists before... 
+  if (previousPlaylist() === undefined) {
+    console.log('new');
+    document.cookie = "playlist=" + playlistArray;
+  } else if (previousPlaylist() !== undefined) {
+    console.log('added');
+    document.cookie = previousPlaylist() + ", " + playlistArray;
+  }
 }
 
 function previousPlaylist() {
@@ -104,29 +107,55 @@ function previousPlaylist() {
   return previousPlaylist;
 }
 
-var previousPlaylistCookie = document.cookie.split('; ').find(row => row.startsWith('playlist'));
-
-function findPlaylist() {
-  // if (urlNumber === 1) {
-  //   console.log("1st");
-  //   var videoArray = previousCookie.substring(4);
-  // } else if (urlNumber > 1) {
-  var playlistArray = (previousPlaylistCookie.substring(4)).split(', ');
-  // }
-  return parseArray(playlistArray);
-}
-
-var playlist = findPlaylist();
-document.getElementById("playlist").innerHTML = playlistArray;
-
-
-
-//this sets the img tag with id 'videoinfo' to have a source = to thumbnail url, resulting in an image showing!
-// document.getElementById("video-thumbnail").src = cookieKeys;
+// function findPlaylists() {
+//   if (playlistNumber === 1) {
+//     console.log("1st");
+//     var playlistLog = playlistCookie.substring(8);
+//   } else if (playlistNumber > 1) {
+//     console.log("2nd");
+//     var playlistLog = (playlistCookie.substring(8)).split(', ');
+//   }
+//   return parsePlaylist(playlistLog);
 // }
 
-//Renders DATA into the div with ID: Demo
-// var readCookie = document.cookie.split(';').map(cookie => cookie.split('='));
 
-//This line takes the 
-// document.getElementById("demo").innerHTML = cookieKeys;
+// function parsePlaylist(array) {
+//   var finalarray = [];
+//   if (playlistNumber === 1) {
+//     finalarray.push(JSON.parse(array));
+//   } else if (playlistNumber > 1) {
+//     for (var i = 0; i < array.length; i++) {
+//       finalarray.push(JSON.parse(`${array[i]}`));
+//     }
+//   }
+//   return finalarray;
+// }
+
+// const playlists = findPlaylists();
+
+
+//Begin on Page Load, Find Data from previous cookie. 
+var playlistCookie = document.cookie.split('; ').find(row => row.startsWith('playlist'));
+if (playlistCookie !== undefined) {
+  var playlistNumber = playlistCookie.match(/"playlistName":/g).length;
+  console.log("# of Playlists Items", playlistNumber);
+} else {
+  playlistNumber = 0;
+}
+
+
+//PLAYLIST SELECTOR
+let dropdownBtn = document.querySelector('.menu-btn');
+let menuContent = document.querySelector('.menu-content');
+dropdownBtn.addEventListener('click', () => {
+  if (menuContent.style.display === "") {
+    menuContent.style.display = "block";
+  } else {
+    menuContent.style.display = "";
+  }
+});
+
+function setPlaylist(word) {
+  selectPlaylist = word;
+  console.log(selectPlaylist);
+}
